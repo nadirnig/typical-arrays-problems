ae6baf25577be7ca201f6ae1f68d7fc1ffcdd928
nadirnig <nadir.pro.sh@gmail.com>
2021-03-09 18:35:31 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  let minimum = Math.min(...array);
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length){
+    return 0;
+  }
+  let maximal = Math.max(...array);
+  return maximal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (!array || !array.length){
+    return 0;
+  }
+  let summa = 0;
+  for ( let i = 0; i < array.length; i++) {
+     summa += array[i] ;    
+
+  }
+  let average = summa / array.length;
+  return average
+  }
+
+
+
+
+
